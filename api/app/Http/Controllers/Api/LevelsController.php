fe5b1b2af6eac019c38977ddd62cc430ec5d53e0
joao.pedroso <joao.pedroso@fretebras.com.br>
2024-06-01 13:05:16 -0300

    feat: implement use helpers in level controller

--- api/app/Http/Controllers/Api/LevelsController.php
+++ api/app/Http/Controllers/Api/LevelsController.php
@@ -2,29 +2,35 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Helpers\RouteParams;
 use App\Http\Controllers\Controller;
 use App\Models\Level;
 use Illuminate\Http\Request;
 
 class LevelsController extends Controller
 {
+
+    private $routeParams;
+
+    public function __construct()
+    {
+        $this->routeParams = new RouteParams();
+    }
+
     /**
      * Display a listing of the resource.
      */
     public function index(Request $request)
     {
 
-        $orderBy = $request->query('orderBy') ?? 'id';
-        $order = $request->query('order') ?? 'asc';
-        $perPage = $request->query('perPage') ?? 10;
-        $query = $request->query('query') ?? '';
+        $queryParams = $this->routeParams->getFilterQueryParams($request);
 
         $results = Level::query()
             ->select(['id', 'nivel'])
-            ->where('nivel', 'LIKE', '%' . $query . '%')
+            ->where('nivel', 'LIKE', '%' . $queryParams['query'] . '%')
             ->withCount('developers')
-            ->orderBy($orderBy, $order)
-            ->paginate($perPage)
+            ->orderBy($queryParams['orderBy'], $queryParams['order'])
+            ->paginate($queryParams['perPage'])
             ->toArray();
 
         $newResults = [
@@ -79,13 +85,18 @@ class LevelsController extends Controller
             $level = Level::find($id);
 
             if ($level->developers()->count() > 0) {
-                throw new \Exception("Não é possivel excluir nível, existem desenvolvedores associados a ele.");
+                throw new \Exception("Não é possivel excluir nível, existem desenvolvedores associados a ele.", 400);
             }
 
             $level->delete();
 
-            return response()->json($level, 200);
-        } catch (\Throwable $th) {
+            return response()->json($level, 204);
+        } catch (\Exception $ex) {
+
+            if ($ex->getCode() === 400) {
+                return response($ex->getMessage(), $ex->getCode());
+            }
+
             return response(null, 400);
         }
     }
