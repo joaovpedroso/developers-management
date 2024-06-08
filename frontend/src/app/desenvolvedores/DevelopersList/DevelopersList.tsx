d11f67ab84fd4f5937ac8eb557239d36d16fb51e
joaovpedroso <jao.victor.dp@gmail.com>
2024-06-08 12:36:42 -0300

    feat: add developers list empty results

--- frontend/src/app/desenvolvedores/DevelopersList/DevelopersList.tsx
+++ frontend/src/app/desenvolvedores/DevelopersList/DevelopersList.tsx
@@ -27,38 +27,43 @@ const DevelopersList = ({
         columns={DEVELOPER_COLUMNS}
         onChangeOrdenation={onChangeOrdenation}
       />
-      <Table.body
-        rows={data.map((row) => (
-          <TableRow key={row.id}>
-            <TableCell align="left">{row.nome}</TableCell>
-            <TableCell align="left">{DeveloperGenre[row.sexo]}</TableCell>
-            <TableCell align="left">{row.idade}</TableCell>
-            <TableCell align="left">
-              {formatDateToLocale(row.data_nascimento)}
-            </TableCell>
-            <TableCell align="left">{row.hobby}</TableCell>
-            <TableCell align="left">
-              <IconAccountTree color="primary" fontSize="small" />{" "}
-              {row.nivel?.nivel}
-            </TableCell>
-            <TableCell align="left">
-              <IconButton
-                aria-label="edit"
-                onClick={() => onSelectItem?.(row.id, ActionEnum.EDIT)}
-              >
-                <EditIcon color="primary" />
-              </IconButton>
 
-              <IconButton
-                aria-label="delete"
-                onClick={() => onSelectItem?.(row.id, ActionEnum.DELETE)}
-              >
-                <DeleteIcon color="error" />
-              </IconButton>
-            </TableCell>
-          </TableRow>
-        ))}
-      />
+      {!data || data.length < 1 ? (
+        <Table.emptyResults tableColumnsLength={7} />
+      ) : (
+        <Table.body
+          rows={data.map((row) => (
+            <TableRow key={row.id}>
+              <TableCell align="left">{row.nome}</TableCell>
+              <TableCell align="left">{DeveloperGenre[row.sexo]}</TableCell>
+              <TableCell align="left">{row.idade}</TableCell>
+              <TableCell align="left">
+                {formatDateToLocale(row.data_nascimento)}
+              </TableCell>
+              <TableCell align="left">{row.hobby}</TableCell>
+              <TableCell align="left">
+                <IconAccountTree color="primary" fontSize="small" />{" "}
+                {row.nivel?.nivel}
+              </TableCell>
+              <TableCell align="left">
+                <IconButton
+                  aria-label="edit"
+                  onClick={() => onSelectItem?.(row.id, ActionEnum.EDIT)}
+                >
+                  <EditIcon color="primary" />
+                </IconButton>
+
+                <IconButton
+                  aria-label="delete"
+                  onClick={() => onSelectItem?.(row.id, ActionEnum.DELETE)}
+                >
+                  <DeleteIcon color="error" />
+                </IconButton>
+              </TableCell>
+            </TableRow>
+          ))}
+        />
+      )}
     </Table.default>
     <Box
       sx={{
